fe72366702428aea5a74d24676ac47144d1a1818
Òscar Herrán <oherranmo@gmail.com>
2022-12-30 22:56:37 +0100

    Implementació Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IniciComponent } from './inici/inici.component';
+import { CatalegComponent } from './cataleg/cataleg.component';
 import { RegistreComponent } from './registre/registre.component';
-
+import { CistellaComponent } from './cistella/cistella.component';
+import {CondicionsComponent} from "./condicions/condicions.component";
+import {ContacteComponent} from "./contacte/contacte.component";
 
 const routes: Routes = [
-  { path: 'registre', component: RegistreComponent }
+
+  { path: '',
+    component:IniciComponent},
+  { path: 'cataleg',
+    component:CatalegComponent},
+  { path: 'registre',
+    component: RegistreComponent },
+  { path: 'cistella',
+    component: CistellaComponent},
+  { path: 'condicions',
+    component: CondicionsComponent },
+  { path: 'contacte',
+    component: ContacteComponent },
+
+
 ];
 
 @NgModule({
