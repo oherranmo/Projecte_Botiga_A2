cbe215248245957134a6bc911e1ca23bcb15486d
Héctor <hector.vinas@institutvidreres.cat>
2023-01-04 20:29:41 +0100

    Cataleg i Cistella 1.0

--- src/app/cataleg/cataleg.component.ts
+++ src/app/cataleg/cataleg.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-cataleg',
   templateUrl: './cataleg.component.html',
   styleUrls: ['./cataleg.component.css','../../assets/css/Default.css']
 })
-export class CatalegComponent {
 
+export class CatalegComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+    let cataleg = document.getElementById("cataleg")
+    console.log(cataleg);
+
+    let generarCataleg =()=>{}
+  }
 }
